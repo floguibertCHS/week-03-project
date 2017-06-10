d1cb3563c4acbe2c3068687daee78e8cc069263b
Flo Guibert <floguibert@Flos-MBP.attlocal.net>
2017-06-10 13:46:24 -0400

    it works!!!!

--- main.js
+++ main.js
@@ -4,31 +4,83 @@ console.log([buttons]);
 var operators = document.querySelectorAll(".operator");
 console.log([operators]);
 
-var inputResult = document.querySelector("#input-result");
+var calculate = document.querySelectorAll(".calculate");
+console.log([calculate]);
 
+var clear = document.querySelectorAll(".clear");
+console.log([clear]);
+
+var inputResult = document.querySelector("#input-result");
 
 var inputVal = [];
 
-for (var i = 0; i<buttons.length; i++){
+// FUNCTION = click event on operators + push button value to array & display on front-end
 
-  buttons[i].addEventListener('click', function(event){
+  for (var i = 0; i<operators.length; i++){
+
+    operators[i].addEventListener('click', function(event){
+
+      console.log('You just clicked', event.target.value);
+
+        inputVal.push(event.target.value);
+
+          inputResult.innerHTML=inputResult.innerHTML+event.target.value;
+
+            var btnVal = this.innerHTML;
+
+              console.log([btnVal]);
+});
+}
+
+// FUNCTION = click event on buttons + push button value to array & display on front-end
+
+  for (var i = 0; i<buttons.length; i++){
+
+    buttons[i].addEventListener('click', function(event){
+
+      console.log('You just clicked', event.target.value);
+
+        // inputVal.push(event.target.value);
+
+          inputResult.innerHTML=inputResult.innerHTML+event.target.value;
+
+            var btnVal = this.innerHTML;
+
+              console.log([btnVal]);
+});
+}
+
+// FUNCTION = calculate when = is clicked
+calculate[0].addEventListener('click',function(event){
 
   console.log('You just clicked', event.target.value);
 
-  inputVal.push(event.target.value);
+    inputVal.unshift(inputResult.innerHTML);
 
-  inputResult.innerHTML=inputResult.innerHTML+event.target.value;
+    console.log([inputVal]);
 
-  console.log([inputVal]);
-});
-}
-var calculate = document.querySelector('#calculate');
-var calculateNOW = function(){
-  if (event.target.value === "=") {
-    eval(inputVal);
-    inputResult.innerHTML="";
-  }
-}
+      var resultString = inputVal[0];
+
+      console.log(resultString);
+
+        var result = eval(resultString);
+
+          console.log(result);
+
+          inputResult.innerHTML=result;
+        });
+
+
+// FUNCTION = clear when C is clicked
+
+clear[0].addEventListener('click',function(event){
+
+            console.log('You just clicked clear', event.target.value);
+
+            inputResult.innerHTML = "";
+
+          });
+
 // for (var i = 0; i<operators.length; i++){
 //
 //   operators[i].addEventListener('click', function(event){
